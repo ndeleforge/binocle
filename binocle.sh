ad357e1f0ec554d76241695e30d2af2df3219b91
ndeleforge <contact@ndeleforge.fr>
2024-11-03 18:20:41 +0100

    Move of launcher scripts

--- binocle.sh
+++ binocle.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 dir=$(dirname "$0")
-venv_dir="${dir}/../venv"
+venv_dir="${dir}/venv"
 
 if [ -d "$venv_dir" ]; then
     . "${venv_dir}/bin/activate"
 fi
 
- python3 "${dir}/../source/binocle.py" "$@"
+ python3 "${dir}/source/binocle.py" "$@"
--- launcher/binocle.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-IF EXIST "%~dp0..\venv" (
-    CALL "%~dp0..\venv\Scripts\activate.bat"
-)
-
-py "%~dp0..\source\binocle.py" %*
